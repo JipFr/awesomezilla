bb46815b663195398a5d64904244524e3a902db8
Jip Fr <jipfrijlink@gmail.com>
2020-05-28 19:51:33 +0200

    Log out & options

--- public/main.js
+++ public/main.js
@@ -732,3 +732,16 @@ window.addEventListener("hashchange", evt => {
 	loopIteration++;
 	init();
 });
+document.addEventListener("click", evt => {
+	// Close detail elements if clicking elsewhere
+	let path = evt.path || [];
+	document.querySelectorAll("details[open]").forEach(el => {
+		if (!path.includes(el)) {
+			el.removeAttribute("open");
+		}
+	});
+});
+document.querySelector(".dropdown .logout").addEventListener("click", () => {
+	localStorage.removeItem("auth");
+	location.reload();
+});
